8bd329aca442ccaa4fd681e33e5ec27c266a631a
tony-vectroda-com <tony@vectroda.com>
2023-04-17 20:53:58 +0100

    reused code fix

---
+++ t4_media/from_db/scripts/scr8_1.js
@@ -0,0 +1,5 @@
+hs.graphicsdir = 'https://cdn.cso.ie/static/img/highslide/graphics/'
+hs.outlinetype = 'rounded-white';
+hs.align = 'center';
+hs.showcredits = false;
+
---
+++ t4_media/from_db/scripts/scr9_1.js
@@ -0,0 +1,6 @@
+
+hs.graphicsdir = 'https://cdn.cso.ie/static/img/highslide/'
+hs.outlinetype = 'rounded-white';
+hs.align = 'center';
+hs.showcredits = false;
+
