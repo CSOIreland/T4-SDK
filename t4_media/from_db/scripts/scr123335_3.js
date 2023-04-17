8439a2262834ce8e5204d0ecd157049287a87379
tony-vectroda-com <tony@vectroda.com>
2023-04-17 07:46:27 +0100

    manually fixed

--- t4_media/from_db/scripts/scr123335_3.js
+++ t4_media/from_db/scripts/scr123335_3.js
@@ -1,13 +1,17 @@
 
-   $(function(){
-   
+$(function () {
+   /*
+original code:
    var publicationtitle = escape("<t4 type="navigation" id="778"/>");
    var chaptertitle = escape("<t4 type="title"/>");
-       
-   if(publicationtitle == chaptertitle) {
-   
-   $( "#chaptertitle" ).hide();
-   } 
-   
-   });    
-        
+   */
+   var publicationtitle = escape('<t4 type="navigation" id="778"/>');
+   var chaptertitle = escape('<t4 type="title"/>');
+
+   if (publicationtitle == chaptertitle) {
+
+      $("#chaptertitle").hide();
+   }
+
+});
+
