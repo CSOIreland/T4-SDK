65fbe7ec68973aba384ef005142a35730791c8fd
tony-vectroda-com <tony@vectroda.com>
2023-04-17 20:37:17 +0100

    reusing scripts

--- t4_media/from_db/scripts/scr1241_1.js
+++
@@ -1,8 +0,0 @@
-
-//hs.graphicsdir = '/images/highslide/graphics/';
-
-hs.graphicsdir = 'https://cdn.cso.ie/static/img/highslide/'
-hs.outlinetype = 'rounded-white';
-hs.align = 'center';
-hs.showcredits = false;
-
--- t4_media/from_db/scripts/scr249428_1.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-             //hs.graphicsdir = '/images/highslide/graphics/';
-             
-            hs.graphicsdir = 'https://cdn.cso.ie/static/img/highslide/'
-    hs.outlinetype = 'rounded-white';
-            hs.align = 'center';
-             hs.showcredits = false;
-             
