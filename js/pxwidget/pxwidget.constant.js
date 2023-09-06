312ecf85aae2123c2a452d6e5d390840aca8ced3
damienderoistecso <damien.roche@cso.ie>
2023-09-06 09:46:21 +0100

    Updates

--- js/pxwidget/pxwidget.constant.js
+++ js/pxwidget/pxwidget.constant.js
@@ -1,5 +1,5 @@
 const T4SDK_PXWIDGET_READ_METADATA = "PxStat.Data.Cube_API.ReadMetadata";
 const T4SDK_PXWIDGET_READ_PRE_METADATA = "PxStat.Data.Cube_API.ReadPreMetadata";
 const T4SDK_PXWIDGET_READ_DATASET = "PxStat.Data.Cube_API.ReadDataset";
-const T4SDK_PXWIDGET_URL_API_PRIVATE = "https://ws.cso.ie/private/api.jsonrpc";
-const T4SDK_PXWIDGET_URL_API_PUBLIC = "https://ws.cso.ie/public/api.jsonrpc";
+const T4SDK_PXWIDGET_URL_API_PRIVATE = "https://dev-ws-netcore.cso.ie/private/api.jsonrpc";
+const T4SDK_PXWIDGET_URL_API_PUBLIC = "https://dev-ws-netcore.cso.ie/public/api.jsonrpc";
