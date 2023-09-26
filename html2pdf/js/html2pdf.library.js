0c74f191e566cb821200453b638cf4d68c380072
Rebecca FitzSmith <rfitzgeraldsmithcso@gmail.com>
2023-09-26 15:00:15 +0100

    master header update pdf api request

--- html2pdf/js/html2pdf.library.js
+++ html2pdf/js/html2pdf.library.js
@@ -84,7 +84,8 @@ t4Sdk.html2pdf.callApiRead = function (input, callback = t4Sdk.html2pdf.callApiR
 t4Sdk.html2pdf.fnExportPDF = function (inp) {
     var toSend = {
         "urls": inp,
-        "htmlIdForMasterHeader": "pageHeaderRowID",
+        "htmlIdForMasterHeader":"pageHeaderRow",
+        "htmlIdsToCut":["pageHeaderRow"],
         "chromeCommandLineOptions": [
             "headless",
             "disable-gpu",
@@ -98,6 +99,7 @@ t4Sdk.html2pdf.fnExportPDF = function (inp) {
         },
         "returnType": "base64String"
     };
+
     var url = "https://pdf.cso.ie/api.jsonrpc";
     var inp2 = { "jsonrpc": "2.0", "method": T4SDK_HTML2PDF_PDF_SERVICE_METHOD_NAME, "params": toSend };
     t4Sdk.html2pdf.callApiRead(inp2, t4Sdk.html2pdf.callApiReadCallback, url);
